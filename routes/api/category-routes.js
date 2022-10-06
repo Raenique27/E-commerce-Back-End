c54405c0597dcd23ab7763f0b75268b38d6a9f57
Raenique27 <raeniquewalters@gmail.com>
2022-10-06 16:45:07 -0400

    re-wrote functions for creating a category, finding one category, and finding all categories

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,53 +3,48 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
-  Category.findAll({
-    include: [{model: Product}],
-  })
-    .then(dbCategoryData => res.json(dbCategoryData))
-    .catch(err => {
-      console.log(err);
-      res.status(500).json(err);
+  try {
+    const dbCategoryData = await Category.findAll({
+      include: [{ model: Product }]
     });
+    res.status(200).json(dbCategoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
-  Category.findOne({
-    where: {
-      id: req.params.id
-    },
-    include: [{model: Product}]
-  })
-    .then(dbCategoryData => {
-      if (!dbCategoryData) {
-        res.status(404).json({ message: 'No category found with this id' });
-        return;
-      }
-      res.json(dbCategoryData);
-    })
-    .catch(err => {
-      console.log(err);
-      res.status(500).json(err);
+  try {
+    const dbCategoryData = await Category.findByPk(req.params.id, {
+      include: [{ model: Product }]
     });
+    if (!dbCategoryData) {
+      res.status(404).json({ message: 'No category found with that id!' });
+      return;
+    }
+    res.status(200).json(dbCategoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+    
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
-  Category.create({
-    category_name: req.body.category_name
-  })
-    .then(dbCategoryData => res.json(dbCategoryData))
-    .catch(err => {
-      console.log(err);
-      res.status(500).json(err);
+  try {
+    const dbCategoryData = await Category.create({
+      category_name: req.body.category_name
     });
+    res.status(200).json(dbCategoryData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
-
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
 });
