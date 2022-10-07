b44e1854b7f250b7131be0d46732c9abc7ff5e69
Raenique27 <raeniquewalters@gmail.com>
2022-10-07 10:13:41 -0400

    added function to update a tag's name

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -33,12 +33,35 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new tag
+  try {
+    const dbTagData = await Tag.create({
+      tag_name: req.body.tag_name
+    });
+    res.status(200).json(dbTagData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
+  try {
+    const dbTagData = await Tag.update(
+      {
+        tag_name: req.body.tag_name
+      },
+      {
+        where: {
+          id: req.params.id
+        }
+      }
+    );
+    res.status(200).json(dbTagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.delete('/:id', (req, res) => {
