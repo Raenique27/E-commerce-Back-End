479ddad09761c6bcc6314051479888e5fd675421
Raenique27 <raeniquewalters@gmail.com>
2022-10-07 10:04:28 -0400

    added function to find all tags and find a single tag

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,14 +3,34 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  try {
+    const dbTagData = await Tag.findAll({
+      include: [{ model: Product }]
+    });
+    res.status(200).json(dbTagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  try {
+    const dbTagData = await Tag.findByPk(req.params.id, {
+      include: [{ model: Product }]
+    });
+    if (!dbTagData) {
+      res.status(404).json({ message: 'No tag found with that id! '});
+      return;
+    }
+    res.status(200).json(dbTagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
